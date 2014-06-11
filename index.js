5d1bc2912c6d54335bd1e4feccc8133bc51f4b74
Jonathan Ong <jonathanrichardong@gmail.com>
2014-06-10 21:59:40 -0700

    don't .close()
    it’s crashing zlib streams and i don’t know why. we’ll renable this
    eventually

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = function (stream) {
   if (stream instanceof Stream) {
     if (typeof stream.destroy === 'function')
       stream.destroy()
-    else if (typeof stream.close === 'function')
-      stream.close()
+    // else if (typeof stream.close === 'function')
+      // stream.close()
   }
 
   return stream
