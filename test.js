6aa6d91730ea3455d02653b777ac2bbd5f020ff4
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-08-14 17:19:45 -0400

    build: run tests on 0.8 and 0.10

--- test.js
+++ test.js
@@ -4,12 +4,18 @@ var destroy = require('./')
 
 describe('Dethroy', function () {
   it('should destroy a stream', function () {
-    var s = fs.createReadStream('package.json')
-    destroy(s)
-    s.destroyed.should.be.ok
+    var stream = fs.createReadStream('package.json')
+    isdestroyed(stream).should.be.false
+    destroy(stream)
+    isdestroyed(stream).should.be.true
   })
 
   it('should handle falsey values', function () {
     destroy()
   })
 })
+
+function isdestroyed(stream) {
+  // readable for 0.8, destroyed for 0.10+
+  return stream.readable === false || stream.destroyed === true
+}
