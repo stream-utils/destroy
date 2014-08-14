460f239d3978d3b7095d3d44ad42f022c29ddd35
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-08-14 17:10:13 -0400

    add fs.ReadStream fd leak work-around

--- test.js
+++ test.js
@@ -13,6 +13,42 @@ describe('Dethroy', function () {
   it('should handle falsey values', function () {
     destroy()
   })
+
+  describe('ReadStream', function () {
+    it('should not leak fd when called sync to open', function (done) {
+      // this test will timeout on a fd leak
+      var _close = fs.close
+      var _open = fs.open
+      var waitclose = false
+
+      function cleanup() {
+        fs.close = _close
+        fs.open = _open
+      }
+
+      fs.close = function close() {
+        _close.apply(this, arguments)
+        cleanup()
+        done()
+      }
+
+      fs.open = function open() {
+        waitclose = true
+        _open.apply(this, arguments)
+      }
+
+      var stream = fs.createReadStream('package.json')
+      destroy(stream)
+      isdestroyed(stream).should.be.true
+
+      if (waitclose) {
+        return
+      }
+
+      cleanup()
+      done()
+    })
+  })
 })
 
 function isdestroyed(stream) {
